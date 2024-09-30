e81c078b5bddce8fdf71424bb7a4dc91f31f6606
Muhammad Waseem Akram <mwaseemakram335@gmail.com>
2024-10-01 01:50:36 +0500

    preparing frontend services

--- backend/routes/auth.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const { signIn } = require('../controllers/authController');
-const router = express.Router();
-
-router.post('/sign-in', signIn);
-
-module.exports = router;
--- backend/routes/carDetails.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { addCarDetails } = require('../controllers/carDetailsController');
-const auth = require('../middleware/auth');
-const { uploadMiddleware } = require('../middleware/uploadMiddleware');
-const router = express.Router();
-
-router.post('/car-details', auth, uploadMiddleware, addCarDetails);
-
-module.exports = router;
